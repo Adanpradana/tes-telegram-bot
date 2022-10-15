b19d4e33a4478e3ce3e77c90531e3ea2f0dcc16c
Adanpradana <adanizkahariz@gmail.com>
2022-10-16 00:40:16 +0700

    test

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ bot.help((ctx) => ctx.reply("Send me a sticker"));
 bot.on("sticker", (ctx) => ctx.reply("👍"));
 bot.hears("hi", (ctx) => ctx.reply("Hey there"));
 bot.launch();
+
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
