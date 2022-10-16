7aad08555fbc91fe0d40a32269d4dad731a07eda
Adanpradana <adanizkahariz@gmail.com>
2022-10-16 20:57:30 +0700

    updated cron job schedule

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
 const { Telegraf } = require("telegraf");
 require("dotenv").config();
 const bot = new Telegraf(process.env.BOT_TOKEN);
+const cron = require("node-cron");
 
-bot.start((ctx) => ctx.reply("Welcome"));
-bot.help((ctx) => ctx.reply("Send me a sticker"));
-bot.on("sticker", (ctx) => ctx.reply("👍"));
-bot.hears("hi", (ctx) => ctx.reply("Hey there"));
+const userId = 1058754017;
+const grupId = -849887444;
+const now = new Date().toDateString();
+const timeNow = new Date().toLocaleTimeString();
+
+const text = `
+jangan lupa minum air ya sekarang,
+sekarang tanggal ${now}
+jam ${timeNow}
+semangat`;
+
+cron.schedule("1 00 09 * * *", () => {
+  bot.telegram.sendMessage(grupId, text);
+});
+cron.schedule("1 00 11 * * *", () => {
+  bot.telegram.sendMessage(userId, text);
+});
+cron.schedule("1 00 13 * * *", () => {
+  bot.telegram.sendMessage(userId, text);
+});
+cron.schedule("1 00 15 * * *", () => {
+  bot.telegram.sendMessage(userId, text);
+});
+cron.schedule("1 00 17 * * *", () => {
+  bot.telegram.sendMessage(userId, text);
+});
+cron.schedule("1 00 19 * * *", () => {
+  bot.telegram.sendMessage(userId, text);
+});
+cron.schedule("1 00 21 * * *", () => {
+  bot.telegram.sendMessage(userId, text);
+});
+
+bot.start((ctx) => {
+  ctx.reply("apa kao");
+});
 bot.launch();
 
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
+console.log("server running...");
