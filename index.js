82c89b81ae8fb4db0240d3b78ad1e39af9e96e76
Adanpradana <adanrizkahariz@gmail.com>
2024-03-31 21:50:01 +0700

    init new bot

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const app = require("express");
+const express = require("express");
 const { Bot, webhookCallback } = require("grammy");
 const cron = require("node-cron");
 const { botStart } = require("./bot");
